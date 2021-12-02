b0edd3575e4465ba104f6bdd94951c4740f36ab6
fkfv <20618646+fkfv@users.noreply.github.com>
2021-12-02 23:18:24 +1300

    Inject size restraint to popup

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
+import {injectGlobal} from '@emotion/css';
 
 import App from './components/app';
 
 import {store} from './redux/store';
 
+
+injectGlobal({
+  'body, #root': {
+    minWidth: '580px',
+    maxWidth: '580px',
+    minHeight: '500px',
+    maxHeight: '500px'
+  }
+});
 
 render(
   <Provider store={store}>
