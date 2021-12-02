196adb12c3f04a34f373953e9f203ddbdea317db
fkfv <20618646+fkfv@users.noreply.github.com>
2021-12-02 20:03:23 +1300

    Create placeholder App component for popup

---
+++ src/popup/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+
+import App from './components/app';
+
+import {store} from './redux/store';
+
+
+render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+, document.getElementById('root'));
